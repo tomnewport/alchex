f9ce041ec819eca26734528bc5abc5a79ac0338f
Tom Newport <tom.newport@gmail.com>
2016-01-28 13:13:30 +0000

    added basic topology concat support, started testing

--- alchex/gromacs_interface.py
+++ alchex/gromacs_interface.py
@@ -474,5 +474,4 @@ class GromacsEditableTOPFile(object):
             for _ in range(moltype_count):
                 unified.topcat(self.moltypes[moltype_name])
         return unified
-
-                            
+      
